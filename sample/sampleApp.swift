c55bb9dbf01a280df7a740b769615ab112f648f3
1N4 <sample@bths.edu>
2025-04-08 16:11:27 -0400

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Student on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
